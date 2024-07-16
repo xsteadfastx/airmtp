c99c206226e34ce80315654f18a81112b3aa4d6c
Marvin Preuss <marvin@xsteadfastx.org>
2024-07-16 11:52:13 +0200

    fix: work around all the local python modules

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Wireless download from your MTP-enabled devices";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs-python.url = "github:cachix/nixpkgs-python";
   };
 
   outputs = { self, nixpkgs, ... }:
@@ -17,16 +16,25 @@
           ];
         };
       };
-      # python = nixpkgs-python.packages.x86_64-linux."2.7.18.8";
     in
     rec {
-      packages.x86_64-linux.airmtp = pkgs.stdenv.mkDerivation {
+      packages.x86_64-linux.airmtp = pkgs.stdenv.mkDerivation rec {
         name = "airmtp";
+        src = ./.;
         dontUnpack = true;
         buildInputs = [ pkgs.python2 ];
-        installPhase = "install -Dm755 ${./airmtp.py} $out/bin/airmtp";
+        installPhase = ''
+          set -x
+          mkdir -p $out/opt/${name}
+          cp -R $src/* $out/opt/${name}
+          chmod +x $out/opt/${name}/${name}.py
+          mkdir $out/bin
+          ln -fs $out/opt/${name}/${name}.py $out/bin/${name}
+          mkdir $out/opt/${name}/appdata
+        '';
       };
 
       packages.x86_64-linux.default = packages.x86_64-linux.airmtp;
     };
 }
+
