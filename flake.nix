9c574b6dce171d98b4fb85eeae53c57caa50e340
Marvin Preuss <marvin@xsteadfastx.org>
2025-04-04 15:33:06 +0200

    feat: flake-utils + backported python nixpkgs inputs

--- flake.nix
+++ flake.nix
@@ -1,36 +1,45 @@
 {
   description = "Wireless download from your MTP-enabled devices";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
 
-  outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        config = { permittedInsecurePackages = [ "python-2.7.18.8" ]; };
-      };
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        py = inputs.nixpkgs-python.packages.${system}."2.7.18";
 
-      airmtp = pkgs.stdenv.mkDerivation rec {
-        name = "airmtp";
-        src = ./.;
-        dontUnpack = true;
-        buildInputs = [ pkgs.python2 ];
-        installPhase = ''
-          set -x
-          mkdir -p $out/opt/${name}
-          cp -R $src/* $out/opt/${name}
-          chmod +x $out/opt/${name}/${name}.py
-          mkdir $out/bin
-          ln -fs $out/opt/${name}/${name}.py $out/bin/${name}
-          mkdir $out/opt/${name}/appdata
-        '';
-      };
-    in {
-      packages.x86_64-linux.airmtp = airmtp;
-      packages.x86_64-linux.default = airmtp;
+        airmtp = pkgs.stdenv.mkDerivation rec {
+          name = "airmtp";
+          src = ./.;
+          dontUnpack = true;
+          buildInputs = [ py ];
+          installPhase = ''
+            set -x
+            mkdir -p $out/opt/${name}
+            cp -R $src/* $out/opt/${name}
+            chmod +x $out/opt/${name}/${name}.py
+            mkdir $out/bin
+            ln -fs $out/opt/${name}/${name}.py $out/bin/${name}
+            mkdir $out/opt/${name}/appdata
+          '';
+        };
+      in
+      {
+        packages.airmtp = airmtp;
+        packages.default = airmtp;
 
-      devShells.x86_64-linux.default =
-        pkgs.mkShell { buildInputs = [ pkgs.python2 airmtp ]; };
-    };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            py
+            airmtp
+          ];
+        };
+      }
+    );
 }
-
